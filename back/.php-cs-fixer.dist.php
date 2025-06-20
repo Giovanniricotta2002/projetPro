b633cfc90224aba92aa175df7189a21f553fe316
Gio <g.ricotta@izitek.net>
2025-06-20 16:59:07 +0200

    main: Updated

--- back/.php-cs-fixer.dist.php
+++ back/.php-cs-fixer.dist.php
@@ -1,13 +1,23 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = (new Finder())
     ->in(__DIR__)
-    ->exclude('var')
+    ->exclude(['var', 'vendor'])
 ;
 
-return (new PhpCsFixer\Config())
+return (new Config())
+    ->setRiskyAllowed(false) // Évite les règles "risquées"
+    ->setIndent('    ') // Indentation en 4 espaces
+    ->setLineEnding("\n") // Fin de ligne UNIX
     ->setRules([
         '@Symfony' => true,
+        'yoda_style' => false,
+        'phpdoc_to_comment' => true,
+        'explicit_string_variable' => true,
+        'concat_space' => ['spacing' => 'one'],
     ])
     ->setFinder($finder)
 ;
