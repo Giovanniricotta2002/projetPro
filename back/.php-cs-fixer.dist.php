3e42861f133c5797efa21b0996f4373e0cfa39e9
Gio <g.ricotta@izitek.net>
2025-07-29 08:25:29 +0200

    main: Updated

--- back/.php-cs-fixer.dist.php
+++ back/.php-cs-fixer.dist.php
@@ -18,6 +18,7 @@ return (new Config())
         'phpdoc_to_comment' => true,
         'explicit_string_variable' => true,
         'concat_space' => ['spacing' => 'one'],
+        'array_syntax' => ['syntax' => 'long'],
     ])
     ->setFinder($finder)
 ;
