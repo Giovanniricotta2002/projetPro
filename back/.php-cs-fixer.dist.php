dd6bcec7b63e859ea1470e93ac81bccf724408b3
Gio <g.ricotta@izitek.net>
2025-08-11 08:16:15 +0200

    feat: Ajouter des règles de formatage pour améliorer la cohérence du code

--- back/.php-cs-fixer.dist.php
+++ back/.php-cs-fixer.dist.php
@@ -19,6 +19,11 @@ return (new Config())
         'explicit_string_variable' => true,
         'concat_space' => ['spacing' => 'one'],
         'array_syntax' => ['syntax' => 'short'],
+        'no_whitespace_before_comma_in_array' => ['after_heredoc' => false],
+        'group_import' => ['group_types' => ['classy']],
+        'single_import_per_statement' => false,
+        'list_syntax' => ['syntax' => 'short']
+        // 'whitespace_after_comma_in_array' => ['ensure_single_space' => true],
     ])
     ->setFinder($finder)
 ;
